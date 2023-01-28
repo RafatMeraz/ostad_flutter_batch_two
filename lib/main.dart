f32f02d5d3f2cef82f3e4fbf9e45c6098b96d09f
RafatMeraz <razhossain201@gmail.com>
2023-01-28 23:40:05 +0600

    flexible, fractionally sizedbox, MediaQuery, Layout builder, Orientation builder

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,114 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Home());
   }
 }
+/// MediaQuery
+/// Layout builder
+/// Orientation builder
+/// Fractional Sizedbox
+/// Flexible
+/// Expanded
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Size size = MediaQuery.of(context).size;
+    // print(size.width);
+    // print(size.height);
+    // print(size.aspectRatio);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      // body: LayoutBuilder(
+      //   builder: (context, constraints) {
+      //     if (constraints.maxWidth < 500) {
+      //       return Center(
+      //         child: Text('Small phone'),
+      //       );
+      //     } else if (constraints.maxWidth < 700) {
+      //       return Center(
+      //         child: Text('Big phone'),
+      //       );
+      //     } else if (constraints.maxWidth < 1000) {
+      //       return Center(
+      //         child: Text('Tablet'),
+      //       );
+      //     } else if (constraints.maxWidth < 1500) {
+      //       return Center(
+      //         child: Text('Desktop'),
+      //       );
+      //     }
+      //     return Center(
+      //       child: Column(
+      //         children: [
+      //           Text(constraints.minWidth.toString()),
+      //           Text(constraints.maxWidth.toString()),
+      //           Text(constraints.maxHeight.toString()),
+      //           Text(constraints.minHeight.toString()),
+      //         ],
+      //       ),
+      //     );
+      //   },
+      // )
+      // body: OrientationBuilder(
+      //   builder: (context, orientation) {
+      //     if (orientation == Orientation.portrait) {
+      //       return Center(child: Text('Portrait'));
+      //     } else {
+      //       return Center(child: Text('Landscape'));
+      //     }
+      //   },
+      // ),
+      // body: SafeArea(
+      //   child: FractionallySizedBox(
+      //     heightFactor: 0.7,
+      //     widthFactor: 0.3,
+      //     child: Container(
+      //       color: Colors.blueGrey,
+      //       child: Center(
+      //         child: Text('Hello'),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 10,
+            child: Container(
+              height: 20,
+              width: 200,
+              color: Colors.deepOrange,),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 5,
+            child: Container(
+              height: 50,
+              width: 200,
+              color: Colors.pink,),
+          ),
+          // SizedBox(height: 50,),
+          // SizedBox(height: 50,),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

5b03ed048f7bb6c4504b517c530058437af4184a
RafatMeraz <razhossain201@gmail.com>
2023-02-06 23:33:19 +0600

    todo list with stateful widget and bottom sheet widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,83 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterScreen(),
+    return const MaterialApp(
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<String> todos = [];
+  TextEditingController todoETController = TextEditingController();
+
+  /// modal bottom sheet - user interaction (anywhere)
+  /// bottom sheet - Body with the help nearest scaffold (show information)
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            barrierColor: Colors.black87,
+            backgroundColor: Colors.green,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20)
+              )
+            ),
+            isScrollControlled: true,
+            enableDrag: true,
+            builder: (context) {
+              return SizedBox(
+                height: 700,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: todoETController,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          todos.add(todoETController.text);
+                          todoETController.clear();
+                          setState(() {});
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Add'),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index]),
+          );
+        },
+      ),
     );
   }
 }

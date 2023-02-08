d4588cd9155cd321f4027b9df9bbf7f482f64ca2
RafatMeraz <razhossain201@gmail.com>
2023-02-08 23:36:22 +0600

    bottom nav bar and pickers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int currentSelectedScreenIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  /// assignment: replace this with 3 screens
+  final List<Widget> widgets = [
+    const Text('Dashboard'), // 0
+    const Text('Products'), // 1
+    const Text('Profile'), // 2
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Bottom nav bar'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                // final selectedTime = await showTimePicker(
+                //     context: context, initialTime: TimeOfDay.now());
+                // print(selectedTime);
+                // final selectedDate = await showDatePicker(context: context,
+                //     initialDate: DateTime.now(),
+                //     firstDate: DateTime(1990),
+                //     lastDate: DateTime(2025));
+                // print(selectedDate);
+                // final selectedDateRange = await showDateRangePicker(
+                //     context: context,
+                //     firstDate: DateTime(2000),
+                //     lastDate: DateTime(2040));
+                // print(selectedDateRange);
+              },
+              icon: const Icon(Icons.lock_clock))
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: widgets[currentSelectedScreenIndex],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueGrey,
+        selectedItemColor: Colors.black87,
+        unselectedItemColor: Colors.black26,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.8,
+            color: Colors.pink),
+        onTap: (value) {
+          currentSelectedScreenIndex = value;
+          setState(() {});
+        },
+        currentIndex: currentSelectedScreenIndex,
+        items: const [
+          BottomNavigationBarItem(
+              backgroundColor: Colors.blue,
+              activeIcon: Icon(Icons.dangerous_outlined),
+              icon: Icon(Icons.dashboard),
+              label: 'Dashboard'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Products'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
     );
   }
 }

549ea1785ff49066b63b229095de34db0720c935
RafatMeraz <razhossain201@gmail.com>
2023-05-10 22:40:16 +0600

    product list ui added

---
+++ lib/ui/screens/product_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:ostad_flutter_batch_two/ui/utils/app_colors.dart';
+import 'package:ostad_flutter_batch_two/ui/widgets/product_card.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+        leading: const BackButton(
+          color: greyColor,
+        ),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, childAspectRatio: 0.75),
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return const ProductCard();
+        },
+      ),
+    );
+  }
+}
--- lib/ui/widgets/category_card_widget.dart
+++ lib/ui/widgets/category_card_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ostad_flutter_batch_two/ui/screens/product_list_screen.dart';
 
 import '../utils/app_colors.dart';
 
@@ -12,37 +14,42 @@ class CategoryCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 6),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                color: primaryColor.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(8)),
-            child: const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Icon(
-                Icons.computer,
-                size: 28,
-                color: primaryColor,
+    return InkWell(
+      onTap: () {
+        Get.to(const ProductListScreen());
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 6),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: primaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8)),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(
+                  Icons.computer,
+                  size: 28,
+                  color: primaryColor,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            name,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontWeight: FontWeight.w400,
-              color: primaryColor,
-              letterSpacing: 0.6,
+            const SizedBox(
+              height: 8,
             ),
-          ),
-        ],
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                color: primaryColor,
+                letterSpacing: 0.6,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
